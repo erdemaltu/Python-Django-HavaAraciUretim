d27ee0f752f395e0f75138f0e39e55a3d782d4e5
erdemaltu <e-altug@hotmail.com>
2024-10-22 00:10:46 +0300

    API docs (swagger)

--- hava_araci_uretim/settings.py
+++ hava_araci_uretim/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_extensions',
     'rest_framework',
+    'drf_yasg',
     'rest_framework.authtoken',
     'personeller',
     'takimlar',
@@ -136,7 +137,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- hava_araci_uretim/urls.py
+++ hava_araci_uretim/urls.py
@@ -16,6 +16,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Hava Aracı Üretim API",
+      default_version='v1',
+      description="Hava aracı üretim takip API belgeleri",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="info@example.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,5 +40,7 @@ urlpatterns = [
     path('api/parcalar/', include('parcalar.api.urls')),
     path('api/envanter/', include('envanter.api.urls')),
     path('', include('frontend.urls')),  # Frontend uygulaması URL'leri
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
